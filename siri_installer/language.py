4950ed8f21cab519fb49aca693e3c0275ff359b1
berce <75228263+must4f@users.noreply.github.com>
2021-05-10 16:29:41 +0300

    Create language.py

---
+++ siri_installer/language.py
@@ -0,0 +1,37 @@
+from . import logo, console, bilgi,hata
+from rich.prompt import Prompt
+from rich.panel import Panel
+from json import loads
+
+def importlang ():
+    COUNTRY = "Turkey"
+    LANGUAGE = "TR"
+    TZ = "Europe/Istanbul"
+    while True:
+        console.clear()
+        logo()
+        console.print(Panel("[blue] :wolf:[/]\n\n[1] Türkçe\n[2] Azərbaycanca\n[3] English"), justify="center")                         
+        Dil = Prompt.ask("[bold yellow]Bir dil seçin / Please select a language[/]", choices=["1", "2", "3"], default="1")
+
+        if Dil == "1":
+            COUNTRY = "Turkey"
+            LANGUAGE = "TR"
+            TZ = "Europe/Istanbul"
+            break
+        elif Dil == "2":
+            COUNTRY = "Azerbaijan"
+            LANGUAGE = "AZ"
+            TZ = "Asia/Baku"
+            break
+        elif Dil == "3":
+            COUNTRY = "United Kingdom"
+            LANGUAGE = "EN"
+            TZ = "Europe/London"
+            break
+        else:
+            hata('\n\n🏴 Wrong choice')
+
+    return COUNTRY, LANGUAGE, TZ
+
+COUNTRY, LANGUAGE, TZ = importlang()
+LANG = loads(open(f"./siri_installer/language/{LANGUAGE}.sirijson", "r").read())["STRINGS"]
