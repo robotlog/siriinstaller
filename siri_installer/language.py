c3b225e8dde37fc223903f049c97119118e4b6cf
berce <75228263+must4f@users.noreply.github.com>
2021-05-11 16:04:13 +0300

    Update language.py

--- siri_installer/language.py
+++ siri_installer/language.py
@@ -1,41 +1,27 @@
 from . import logo, console, bilgi, hata, soru
+from rich.prompt import Prompt
 from rich.panel import Panel
 from time import sleep
 from json import loads
 
 def importlang ():
-    COUNTRY = "Turkey"
-    LANGUAGE = "TR"
-    TZ = "Europe/Istanbul"
-    while True:
-        console.clear()
-        logo()
-        bilgi("[blue]\n\n[1] Türkçe\n[2] Azərbaycanca\n[3] English[/]")
-        try:   
-            Dil = int(soru("[bold yellow]Bir dil seçin / Please select a language[/]"))
-        except ValueError:
-            hata('🔢 Just Enter Number! / Sadece Sayı! / Yalnız nömrə!')
-            sleep(2)
+    console.clear()
+    logo()
+    bilgi("[blue]\n\n[1] Türkçe\n[2] Azərbaycanca\n[3] English[/]")
+    Dil = Prompt.ask("[bold yellow]Bir dil seçin / Please select a language[/]", choices=["1", "2", "3"], default="1")
 
-        if Dil == "1":
-            COUNTRY = "Turkey"
-            LANGUAGE = "TR"
-            TZ = "Europe/Istanbul"
-            break
-        elif Dil == "2":
+    if Dil == "1":
+        COUNTRY = "Turkey"
+        LANGUAGE = "TR"
+        TZ = "Europe/Istanbul"
+    elif Dil == "2":
             COUNTRY = "Azerbaijan"
             LANGUAGE = "AZ"
             TZ = "Asia/Baku"
-            break
-        elif Dil == "3":
+    elif Dil == "3":
             COUNTRY = "United Kingdom"
             LANGUAGE = "EN"
             TZ = "Europe/London"
-            break
-        else:
-            console.clear()
-            hata('🏴 Wrong choice! / Yanlış seçim! / Səhv seçim!')
-            sleep(2)
 
     return COUNTRY, LANGUAGE, TZ
 
